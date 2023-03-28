5d3b9b635cf096160cd9b232cb52db43cd4700fc
David Zar <zardav@gmail.com>
2023-03-28 19:44:11 +0300

    Create utils.py with run_process

---
+++ utils.py
@@ -0,0 +1,16 @@
+from subprocess import Popen, PIPE, STDOUT
+
+
+def run_process(args, shell=False, **kwargs)
+    kwargs.update({
+        'args': args,
+        'shell': shell,
+        'stdout': PIPE,
+        'stderr': STDOUT,
+        'close_fds': True
+    })
+    process = Popen(**kwargs))
+    for line in iter(process.stdout.readline, b''):
+        print(line.rstrip().decode('utf-8'))
+    process.stdout.close()
+    return process.wait()
