138fc8a422552d3f51d6a426cfa140015d8f0dd9
David Zar <zardav@gmail.com>
2023-03-28 20:10:51 +0300

    Add option to choose num_inference_steps

--- make_class_images.py
+++ make_class_images.py
@@ -6,7 +6,8 @@ from tqdm import tqdm
 import fire
 
 
-def batch_make(load_dir: str, save_dir: str, images_per_prompt: int = 4, replacement: Tuple[str, str] = ("", ""), batch_size = 4):
+def batch_make(load_dir: str, save_dir: str, images_per_prompt: int = 4, replacement: Tuple[str, str] = ("", ""), 
+               batch_size = 4, guidance_scale=7, num_inference_steps=25):
     model_id = "stabilityai/stable-diffusion-2-1-base"
     scheduler = EulerAncestralDiscreteScheduler.from_pretrained(model_id, subfolder="scheduler")
     pipe = StableDiffusionPipeline.from_pretrained(model_id, scheduler=scheduler, torch_dtype=torch.float16)
@@ -23,7 +24,8 @@ def batch_make(load_dir: str, save_dir: str, images_per_prompt: int = 4, replace
         images = []
         while len(images) < images_per_prompt:
             n_images = min(batch_size, images_per_prompt-len(images))
-            images += pipe(prompt, guidance_scale=7, negative_prompt=negative_prompt, num_images_per_prompt=n_images).images
+            images += pipe(prompt, guidance_scale=guidance_scale, negative_prompt=negative_prompt, 
+                           num_images_per_prompt=n_images, num_inference_steps=num_inference_steps).images
         for i, image in enumerate(images):
             save_name = f"{file_path.name}-{i}"
             with open(save_dir/f"{save_name}.txt", 'w') as f:
